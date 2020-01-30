5f64d9e3d67f1372623e236d9272a3303d655f2a
aylin <aylin.durur@derinarge.com>
2020-01-30 10:11:11 +0300

    login sayfası textboxa yazılan değerlşeri console da alınıyor.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {first} from 'rxjs/operators';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  loginForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder) {
+    this.createForm();
   }
 
+  createForm(){
+    this.loginForm = this.formBuilder.group({
+      username: ['' , Validators.required ],
+      password: ['' , Validators.required ]
+    });
+  }
+
+  ngOnInit() {
+   
+  }
+
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
+
+
+
+
+

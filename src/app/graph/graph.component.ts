e54d2bdb6364ce8fc6dab83327bbdb117ffdf437
aylin <aylin.durur@derinarge.com>
2020-02-05 13:08:43 +0300

    xml verisi çekilip tabloya basıldı

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ExchangeService} from '../service/exchange.service';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-graph',
@@ -10,15 +11,15 @@ import {ExchangeService} from '../service/exchange.service';
 })
 export class GraphComponent implements OnInit {
   exchangeForm: FormGroup;
-
-  constructor(private formBuilder: FormBuilder, private exchangeService: ExchangeService) {
-  }
-
+  data: boolean = true;
   exchangeResult: any;
+
+  constructor(private formBuilder: FormBuilder, private exchangeService: ExchangeService, private http: HttpClient) {
+  }
 
   ngOnInit() {
     this.exchangeForm = this.formBuilder.group({
-      selectedDate: ['', Validators.required]
+      startDate: ['', Validators.required]
     });
   }
 
@@ -28,9 +29,13 @@ export class GraphComponent implements OnInit {
 
   showExchange(): void {
     console.log(this.exchangeForm.value);
-    this.exchangeService.getExchange(this.f.selectedDate.value).subscribe(x => {
+    this.exchangeService.getExchange(this.f.startDate.value).subscribe(x => {
+      this.exchangeService.parseXML(x)
+        .then((data) => {
+          this.exchangeService.xmlItems = data;
+        });
       this.exchangeResult = x;
-      console.log('sonuc :' , this.exchangeResult);
+      console.log('sonuc :', this.exchangeResult);
     });
   }
 }
--- src/app/service/exchange.service.ts
+++ src/app/service/exchange.service.ts
@@ -1,22 +1,67 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import xml2js from 'xml2js';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ExchangeService {
+  public xmlItems: any;
 
-  constructor( private http: HttpClient) {
+  constructor(private http: HttpClient) {
+    // const headers = new HttpHeaders({'Content-Type': 'text/xml'}).set('Accept', 'text/xml');
   }
 
-  getExchange(currentDate: string) {
-    const yyyy = currentDate.split('-')[0];
-    const MM = currentDate.split('-')[1];
-    const dd = currentDate.split('-')[2];
+  getExchange(startDate: string) {
+    const yyyy = startDate.split('-')[0];
+    const MM = startDate.split('-')[1];
+    const dd = startDate.split('-')[2];
     return this.http.get(
       'kurlar/' + yyyy + MM + '/' + dd + MM + yyyy + '.xml',
-      {responseType: 'text'}
-    );
+      {
+        headers: new HttpHeaders()
+          .set('Content-Type', 'text/xml')
+          .append('Access-Control-Allow-Methods', 'GET')
+          .append('Access-Control-Allow-Origin', '*')
+          // tslint:disable-next-line:max-line-length
+          .append('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method'),
+        responseType: 'text'
+      });
+  }
+
+  parseXML(data) {
+    return new Promise(resolve => {
+      // tslint:disable-next-line:one-variable-per-declaration
+      let k: string | number,
+        // tslint:disable-next-line:prefer-const
+        arr = [],
+        // tslint:disable-next-line:prefer-const
+        parser = new xml2js.Parser(
+          {
+            trim: true,
+            explicitArray: true
+          });
+      // tslint:disable-next-line:only-arrow-functions
+      parser.parseString(data, function(err, result) {
+        const obj = result.Tarih_Date;
+        // tslint:disable-next-line:forin
+        for (k in obj.Currency) {
+          const item = obj.Currency[k];
+          arr.push({
+            Unit: item.Unit[0],
+            Isim: item.Isim[0],
+            CurrencyName: item.CurrencyName[0],
+            ForexBuying: item.ForexBuying[0],
+            ForexSelling: item.ForexSelling[0],
+            BanknoteBuying: item.BanknoteBuying[0],
+            BanknoteSelling: item.BanknoteSelling[0],
+            CrossRateUSD: item.CrossRateUSD[0],
+            CrossRateOther: item.CrossRateOther[0]
+          });
+        }
+        resolve(arr);
+      });
+    });
   }
 }
